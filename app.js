36ad5b1c046ba9b00586fc75d0d4cfdb0b4952ba
b1gno <slorenzo.bignotti@itis.pr.it>
2026-04-30 11:39:14 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Configurazione per leggere dati dai form e JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Cartella per file statici (CSS, Immagini)
+app.use(express.static('public'));
+
+// Motore di template
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Rotta di prova
+// Trova questa parte nel tuo app.js e modificala così:
+app.get('/', (req, res) => {
+    res.render('index'); // Questo cercherà il file views/index.ejs
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server attivo su http://localhost:${PORT}`);
+});
