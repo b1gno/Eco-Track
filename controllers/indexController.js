5877c64a93d9f12e1cd51120203f5d3fe869d693
Dragonspirit07 <smatteo.fornaciari@itis.pr.it>
2026-05-02 15:42:53 +0200

    Home data from json

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,29 @@
 const impactModel = require('../models/impactModel');
+const fs = require('fs');
+const path = require('path');
+ 
+// Leggi i dati una volta all'avvio
+const consigli = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../data/consigli.json'), 'utf8')
+);
+const emissioni = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../data/emissioni.json'), 'utf8')
+);
+const impatti = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../data/impatti.json'), 'utf8')
+);
  
 exports.getHome = (req, res) => {
-    res.render('index');
+  res.render('index', { consigli, emissioni, impatti });
 };
  
 exports.getCalcola = (req, res) => {
-    res.render('calcola', { risultato: null, km: null, pasto: null });
+  res.render('calcola', { risultato: null, km: null, pasto: null });
 };
  
 exports.postCalcola = (req, res) => {
-    const { km, pasto } = req.body;
-    const calcolo = impactModel.calculateImpact(parseFloat(km), pasto);
-    res.render('calcola', { risultato: calcolo, km, pasto });
+  const { km, pasto } = req.body;
+  const calcolo = impactModel.calculateImpact(parseFloat(km), pasto);
+  res.render('calcola', { risultato: calcolo, km, pasto });
 };
+ 
